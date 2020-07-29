963955f02083e77645524d2f6d7b06b55e47efc9
Pranav Gupta <pranav.gupta.cse@gmail.com>
2020-07-29 14:42:57 -0700

    Update deleteMiddleNode.py

--- Data Structures/Linked Lists/deleteMiddleNode.py
+++ Data Structures/Linked Lists/deleteMiddleNode.py
@@ -2,3 +2,15 @@
 Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but the first and last node, not necessarily the exact middle) 
 of a singly linked list, given only access to that node.
 """
+
+def deleteMiddleNode(node):
+    """
+    Since we do not have access to the head, the simplest way is to copy the data of next node to the node we want to delete, and then delete the next node.
+    Time complexity: O(1)
+    Space complexity: O(1)
+    """
+    if node == None or node.next == None:
+        return False
+    node.val = node.next.val
+    node.next = node.next.next
+    return True
