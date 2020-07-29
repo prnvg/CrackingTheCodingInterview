553898e7495fd66afe7849ae120e51e9140b71af
Pranav Gupta <pranav.gupta.cse@gmail.com>
2020-07-28 18:46:23 -0700

    Update checkPermutation.py

--- Data Structures/Arrays and Strings/checkPermutation.py
+++ Data Structures/Arrays and Strings/checkPermutation.py
@@ -2,6 +2,14 @@
 
 import collections
 def checkPermutation(string1, string2):
+    """
+    Counting the number of occurrence of each letter in both strings and comparing the count of corresponding letters.
+    If the count is same for each letter, then the strings are permutations, otherwise not.
+    Time complexity: O(n)
+    Space complexity: O(n)
+    where n is assumed to be the length of each string.
+    If the lengths are different, then the two strings are definitely not permutations, and we can simply return False.
+    """
     if len(string1) != len(string2):
         return False
     counter1 = collections.Counter(string1)
@@ -14,6 +22,14 @@ def checkPermutation(string1, string2):
     return True
 
 def checkPermutation1(string1, string2):
+    """
+    Sorting both the strings and then checking if the two resulting strings are the same.
+    If they are exactly same, then the strings are permutations, otherwise not.
+    Time complexity: O(nlogn)
+    Space complexity: O(1)
+    where n is assumed to be the length of each string.
+    If the lengths are different, then the two strings are definitely not permutations, and we can simply return False.
+    """
     if len(string1) != len(string2):
         return False
     string1 = sorted(string1)
@@ -21,7 +37,12 @@ def checkPermutation1(string1, string2):
     
     return string1 == string2
 
+#Testing...
 string1 = "abba"
-string2 = "aaba"
+string2 = "bbaa"
+string3 = "aaab"
 
-checkPermutation1(string1, string2)
+checkPermutation(string1, string2) #True
+checkPermutation1(string1, string2) #True
+checkPermutation(string1, string3) #False
+checkPermutation1(string1, string3) #False
